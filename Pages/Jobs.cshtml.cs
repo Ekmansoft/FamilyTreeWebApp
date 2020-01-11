5a4eb3401ff23ec07260321fe30544d1703259c2
Kenneth Ekman <endian02@endian.net>
2020-01-11 11:34:55 +0100

    Fixed recent bug

---
+++ Pages/Jobs.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCoreWebApp3.Pages
+{
+  [AllowAnonymous]
+  public class JobModel : PageModel
+  {
+    public string Message { get; set; }
+
+    public IActionResult OnGet()
+    {
+      Message = "Improve your family tree.";
+      return Redirect("/FamilyTree/AnalysisResultView/Index");
+    }
+  }
+}
